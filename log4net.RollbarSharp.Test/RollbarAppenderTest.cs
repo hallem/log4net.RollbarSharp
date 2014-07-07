32c52a9c4f687130954a80c9068affbed9122958
Michael Hallett <michael@toutapp.com>
2014-07-07 14:04:51 -0700

    Created initial implementation of the log4net appender for Rollbar

---
+++ log4net.RollbarSharp.Test/RollbarAppenderTest.cs
@@ -0,0 +1,127 @@
+﻿using System.Threading;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using log4net.Util;
+using NUnit.Framework;
+using RollbarSharp;
+
+namespace log4net.RollbarSharp.Test
+{
+    [TestFixture]
+    public class RollbarAppenderTest
+    {
+        [TestFixtureSetUp]
+        public void SetUpFixture()
+        {
+            Util.LogLog.InternalDebugging = true;
+
+            Hierarchy hierarchy = (Hierarchy) LogManager.GetRepository();
+            PatternLayout patternLayout = new PatternLayout { ConversionPattern = "%d [%t] %-5p %m%n" };
+
+            patternLayout.ActivateOptions();
+
+            TraceAppender tracer = new TraceAppender { Layout = patternLayout };
+
+            tracer.ActivateOptions();
+
+            hierarchy.Root.AddAppender(tracer);
+
+            ConsoleAppender console = new ConsoleAppender
+                                          {
+                                              Name = "console",
+                                              Layout = patternLayout,
+                                              Target = "Console.Out",
+                                              Threshold = Level.All,
+                                          };
+
+            console.ActivateOptions();
+
+            hierarchy.Root.AddAppender(console);
+
+            RollingFileAppender rollingFile = new RollingFileAppender
+                                                  {
+                                                      Name = "file",
+                                                      Layout = patternLayout,
+                                                      AppendToFile = true,
+                                                      RollingStyle = RollingFileAppender.RollingMode.Size,
+                                                      MaxSizeRollBackups = 4,
+                                                      MaximumFileSize = "100KB",
+                                                      StaticLogFileName = true,
+                                                      LockingModel = new FileAppender.MinimalLock(),
+                                                      File = "logs\\logFile.txt",
+                                                  };
+
+            rollingFile.ActivateOptions();
+
+            hierarchy.Root.AddAppender(rollingFile);
+
+            RollbarAppender rollbar = new RollbarAppender
+                                          {
+                                              Name = "rollbar",
+                                              Layout = patternLayout,
+                                              AccessToken = "3203880e148b43b4b1a14430fb41957a",
+                                              Threshold = Level.Notice,
+                                          };
+
+            rollbar.ActivateOptions();
+
+            hierarchy.Root.AddAppender(rollbar);
+
+            hierarchy.Root.Level = Level.All;
+            hierarchy.Configured = true;
+        }
+
+        private bool wait = true;
+
+        [Test]
+        public void LogToRollbar()
+        {
+            ILog logger = LogManager.GetLogger(typeof(RollbarAppenderTest));
+
+            logger.Debug("This is a debug message");
+            logger.Info("This is an info message");
+            logger.Warn("This is a warning message");
+            logger.Error("This is an error message");
+            logger.Fatal("This is a fatal message");
+
+            Thread.Sleep(30 * 1000);
+        }
+
+        [Test]
+        public void RawLogToRollbar()
+        {
+            var client = new RollbarClient("3203880e148b43b4b1a14430fb41957a");
+
+            client.RequestStarting += this.RollbarClient_RequestStarting;
+            client.RequestCompleted += this.RollbarClient_RequestCompleted;
+
+            var notice = client.NoticeBuilder.CreateMessageNotice("This is a test message");
+
+            notice.Level = "error";
+            notice.Title = "Test Message";
+
+            client.Send(notice);
+
+            while (wait) { }
+        }
+
+        private void RollbarClient_RequestCompleted(object source, RequestCompletedEventArgs args)
+        {
+            if (args.Result.IsSuccess)
+                LogLog.Debug(typeof(RollbarAppender), "Request was successful: " + args.Result.Message);
+            else
+                LogLog.Error(typeof(RollbarAppender), "Request failed: " + args.Result);
+
+            wait = false;
+        }
+
+        private void RollbarClient_RequestStarting(object source, RequestStartingEventArgs args)
+        {
+            var client = (RollbarClient) source;
+
+            LogLog.Debug(typeof(RollbarAppender), string.Format("Sending request to {0}: {1}", client.Configuration.Endpoint, args.Payload));
+        }
+    }
+}
---
+++ log4net.RollbarSharp/RollbarAppender.cs
@@ -0,0 +1,121 @@
+﻿using log4net.Appender;
+using log4net.Core;
+//using log4net.Layout;
+using log4net.Util;
+using RollbarSharp;
+
+namespace log4net.RollbarSharp
+{
+    public class RollbarAppender : AppenderSkeleton
+    {
+        #region Public Instance Properties
+
+        public string AccessToken { get; set; }
+
+        //public string Endpoint { get; set; }
+
+        //public PatternLayout Environment { get; set; }
+
+        //public PatternLayout Platform { get; set; }
+
+        //public PatternLayout Language { get; set; }
+
+        //public PatternLayout Framework { get; set; }
+
+        #endregion
+
+        #region Override implementation of AppenderSkeleton
+
+        /// <summary>
+        /// This method is called by the <see cref="M:AppenderSkeleton.DoAppend(LoggingEvent)"/> method.
+        /// </summary>
+        /// <param name="loggingEvent">The event to log.</param>
+        /// <remarks>
+        /// <para>
+        /// Writes the event to Rollbar.
+        /// </para>
+        /// <para>
+        /// The format of the output will depend on the appender's layout.
+        /// </para>
+        /// </remarks>
+        override protected void Append(LoggingEvent loggingEvent)
+        {
+            var client = new RollbarClient(this.AccessToken);
+
+            client.RequestStarting += this.RollbarClient_RequestStarting;
+            client.RequestCompleted += this.RollbarClient_RequestCompleted;
+
+            //if (!string.IsNullOrWhiteSpace(this.Endpoint))
+            //    client.Configuration.Endpoint = this.Endpoint;
+
+            //if (this.Environment != null)
+            //    client.Configuration.Environment = this.Environment.Format(loggingEvent);
+
+            //if (this.Platform != null)
+            //    client.Configuration.Platform = this.Platform.Format(loggingEvent);
+
+            //if (this.Language != null)
+            //    client.Configuration.Language = this.Language.Format(loggingEvent);
+
+            var notice = loggingEvent.ExceptionObject != null
+                ? client.NoticeBuilder.CreateExceptionNotice(loggingEvent.ExceptionObject)
+                : client.NoticeBuilder.CreateMessageNotice(base.RenderLoggingEvent(loggingEvent));
+
+            notice.Level = ConvertLogLevel(loggingEvent.Level);
+            //notice.Title = string.Empty;
+
+            client.Send(notice);
+        }
+
+        private void RollbarClient_RequestCompleted(object source, RequestCompletedEventArgs args)
+        {
+            if (args.Result.IsSuccess)
+            {
+                LogLog.Debug(typeof(RollbarAppender), "Request was successful: " + args.Result.Message);
+                return;
+            }
+
+            LogLog.Warn(typeof(RollbarAppender), "Request failed: " + args.Result);
+        }
+
+        private void RollbarClient_RequestStarting(object source, RequestStartingEventArgs args)
+        {
+            var client = (RollbarClient) source;
+
+            LogLog.Debug(typeof(RollbarAppender), string.Format("Sending request to {0}: {1}", client.Configuration.Endpoint, args.Payload));
+        }
+
+        /// <summary>
+        /// This appender requires a <see cref="Layout"/> to be set.
+        /// </summary>
+        /// <value><c>true</c></value>
+        /// <remarks>
+        /// <para>
+        /// This appender requires a <see cref="Layout"/> to be set.
+        /// </para>
+        /// </remarks>
+        override protected bool RequiresLayout
+        {
+            get { return true; }
+        }
+
+        #endregion Override implementation of AppenderSkeleton
+
+        private static string ConvertLogLevel(Level level)
+        {
+            if (level.Value > Level.Error.Value)
+                return "critical";
+
+            if (level.Value == Level.Error.Value)
+                return "error";
+
+            if (level.Value > Level.Notice.Value)
+                return "warning";
+
+            if (level.Value == Level.Info.Value)
+                return "info";
+
+            return "debug";
+        }
+    }
+}
